60e006361529af47d58edbd53f29f304a9714211
Jerome Marshall <jeromemarshall0@gmail.com>
2021-05-04 22:08:16 +0530

    finalizing

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 
-const connectionUrl = 'mongodb://127.0.0.1:27017/task-manager'
-
-mongoose.connect(connectionUrl, {
+mongoose.connect(process.env.MONGODB_CONNECTION_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
+}).catch( e => {
+    console.log(e)
 })
 
